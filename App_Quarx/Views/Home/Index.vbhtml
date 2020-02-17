3709721ffac178d98a77da5aba349332f27abf25
Destroyer <joaquin121416@gmail.com>
2020-02-17 17:37:22 -0500

    Primer Commit de Coti/App/Oauth

---
+++ App_Quarx/Views/Home/Index.vbhtml
@@ -0,0 +1,9 @@
+﻿@Code
+    ViewData("Title") = "Home Page"
+End Code
+
+    <div class="page-wrapper">
+        Me la pelan
+
+
+    </div>
---
+++ Ms_Cotizacion/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace Ms_Cotizacion
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32 + (int (float this.TemperatureC / 0.5556))
